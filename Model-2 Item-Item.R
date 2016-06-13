484d784f2aa07524256ced9981e53471e1e0b141
PrakashR <vanapaliprakash@gmail.com>
2016-06-13 14:29:39 +0530

    Update Model-2 Item-Item.R

--- Model-2 Item-Item.R
+++ Model-2 Item-Item.R
@@ -1,16 +1,20 @@
 # Set the Working data
-# The data file is attached with this folder. 
 
 #Load the Libraries 
 library(dplyr)
 library(plyr)
 
+# Assumptions 
+# 1. We are Using Pearson-Correlation to find the Item-Item Similarity.
+# 2. We are Using the Similarity to find the results. We have scaled the data with mean before finding the
+#   correlation
+
 #Laod the data
-load("ratingsdummydata")
+ratingsdummydata <- read.csv("ratingsdummydata.csv",sep = ",",header = TRUE)
 
 #Create a dummy martix 
 movies <- as.list(colnames(ratingsdummydata)[2:11])
-pearson_correlation <- matrix(data = NA , nrow = 10, ncol = 10, dimnames = list(c(movies),c(movies)))
+pearson_correlation <- matrix(data = NA , nrow = ncol(ratingsdummydata)-1, ncol = ncol(ratingsdummydata)-1, dimnames = list(c(movies),c(movies)))
 
 #Creating another data frame by removing users from the previous ratingsdummydata
 Itemratingsdummydata <- ratingsdummydata[,-1]
@@ -23,9 +27,9 @@ for(i in 1:length(Itemratingsdummydata)){
     P2 <- Itemratingsdummydata[,j]
     data <- cbind(P1,P2)
     data <- data.frame(data)
+    data <- na.omit(data)
     mean1 <- mean(data$P1,na.rm = TRUE)
     mean2 <- mean(data$P2,na.rm = TRUE)
-    data <- na.omit(data)
     data$P1 <- data$P1 - mean1
     data$P2 <- data$P2 - mean2
     data <- mutate(data, P1P2 = P1*P2)
@@ -54,7 +58,12 @@ for ( i in 1:nrow(Predictions)){
       matrix1row <- sort(matrix1row,decreasing = TRUE)
       m <- Itemratingsdummydata[i,c(names(matrix1row))]
       z <- which(is.na(Itemratingsdummydata[i,c(names(matrix1row))]))
-      m <-m[-z]
+      if(length(z) == 0){
+        m <- m
+      }
+      else{
+        m <-m[-z]
+      }
       meanmovie <- mean(Itemratingsdummydata[,j],na.rm=TRUE)
       Predictions[i,j]<-meanmovie+((pearson_correlation[j,names(m)] %*% t(Itemratingsdummydata[i,names(m)]-meanmovie)))/sum(abs(pearson_correlation[j,names(m)]))
     }
@@ -69,8 +78,6 @@ Predictions <- cbind(ratingsdummydata[,1],Predictions)
 colnames(Predictions)[1] <- "User"
 
 
-
-# Code for Finding Individual User Predicted best Movies.
 # Two DataFrames
 # Itemsratingsdummydata - User rated ratings
 # Predictions           - User Predicted Ratings
@@ -90,6 +97,4 @@ Username <- function(x){
   }
 }
 
-# This Code works fine and needs to be improved when it comes to system automatically telling User Predicted Best movies as 
-# When the User name is entered.
-# Working on ShinyApp now.
+# Only with number it is working 
